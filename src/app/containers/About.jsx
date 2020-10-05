a8d9ba571e50052379ba3432b7d4cfc9668bfbc4
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-05 23:45:57 +0330

    add font

--- src/app/containers/About.jsx
+++ src/app/containers/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => {
-  return (<a className="text-danger" href="https://github.com/KavehKarami" target="_blank" rel="noopener noreferrer">Create by Kaveh Karami</a>);
+  return (<a className="text-danger" href="https://github.com/KavehKarami" target="_blank" rel="noopener noreferrer">Create2 by Kaveh Karami</a>);
 }
 
 export default About;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./app/App";
+import "./assets/fonts/fontFace.scss";
 import "./assets/scss/main.scss";
 
 ReactDOM.render(
